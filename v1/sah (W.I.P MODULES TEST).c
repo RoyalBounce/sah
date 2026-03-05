cbf52abf97a36cdfd0f709b725d6eababec4daa9
0x7C00 <137730237+royalbyte@users.noreply.github.com>
2026-03-05 15:39:48 -0300

    Update sah (W.I.P MODULES TEST).c

--- v1/sah (W.I.P MODULES TEST).c
+++ v1/sah (W.I.P MODULES TEST).c
@@ -16,40 +16,32 @@
 #define STACK_LARGE (8 * MEGABYTE)
 #define STACK_HUGE (16 * MEGABYTE)
 
-/* API */
 struct _stack_header {
 	size_t size;
 };
 
-/* STACK CONTROLLER STRUCTURE, ALLOCATED ON THE HEAP AND TRACKS THE SIZE OF THE STACK */
 struct sah_stack {
 	size_t payload_size;
 	uint8_t* bp;
 	uint8_t* sp;
 }; __attribute__((aligned(64)));
 
-/* BEGIN PUBLIC API FUNCTIONS SIGNATURE */
 struct sah_stack* screate(size_t);
 void sdestroy(struct sah_stack*);
 void* spush(struct sah_stack*, size_t);
 void spop(struct sah_stack*);
-/* END PUBLIC API FUNCTIONS SIGNATURE */
 
-/* BASIC MODULE STATIC FUNCTIONS */
-/* FUNCTION push(), PUSHES INTO THE STACK, MANUAL CONTROL OF SIZE TO PUSH */
 static inline void* push(struct sah_stack* s, size_t n)
 {
 	s->sp -= n;
 	return s->sp;
 }
 
-/* FUNCTION pop(), POP THE STACK, MANUAL CONTROL OF SIZE TO POP */
 static inline void pop(struct sah_stack* s, size_t n)
 {
 	s->sp += n;
 }
 
-/* FUNCTION sreset(), RESET THE STACK MOVING THE SP TO BP, ALLOWING REUSE */
 static inline void sreset(struct sah_stack* s)
 {
 	s->sp = s->bp;
@@ -61,7 +53,6 @@ static inline void sreset(struct sah_stack* s)
 
 #ifdef BASIC
 
-/* FUNCTION screate(), CREATES THE STACK AND RETURNS THE STACK CONTROLLER/HANDLER FOR USE */
 struct sah_stack* screate(size_t psize)
 {
 	size_t guard = sysconf(_SC_PAGESIZE);
@@ -85,7 +76,6 @@ struct sah_stack* screate(size_t psize)
 	return s;
 }
 
-/* FUNCTION sdestroy(), DESTROY THE STACK GIVEN A STACK CONTROLLER/HANDLER, FREE ALL MEMORY INCLUDING THE STACK CONTROLLER */
 void sdestroy(struct sah_stack* s)
 {
 	if (s == NULL) return;
@@ -100,7 +90,6 @@ void sdestroy(struct sah_stack* s)
 	free(s);
 }
 
-/* FUNCTION spush(), PUSH INTO THE STACK WITH HEADERS TO TRACK SIZE FOR SPOP */
 void* spush(struct sah_stack* s, size_t n)
 {
 	size_t rtotal = sizeof(struct _stack_header) + n;
@@ -114,7 +103,6 @@ void* spush(struct sah_stack* s, size_t n)
 	return (void*)(hdr + 1);
 }
 
-/* FUNCTION spop(), POP THE STACK, AUTO TRACKS THE AMOUNT TO POP WITH spush() HEADER */
 void spop(struct sah_stack* s)
 {
 	struct _stack_header* hdr = (struct _stack_header*)s->sp;
